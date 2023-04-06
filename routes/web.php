244375efa18937890cf7cd6c04532321f5cca94b
hdjfu <hdjfulucas@gmail.com>
2023-04-06 13:25:56 -0300

    Alteração

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\TryCatch;
 
 /*
 |--------------------------------------------------------------------------
@@ -62,8 +63,48 @@ Route::get('/conta/{n1}/{n2}/{operação?}', function ($n1, $n2, $operacao=null)
 
 Route::get('/idade/{ano}/{mes?}/{dia?}', function ($ano, $mes=null, $dia=null) {
 
-    
+    $dataAtual = date('Y-m-d');
+    global $data;
+    $data = new DateTime();
+    function setDate($dateString){
+        try {
+            global $data;
+            $data = new DateTime($dateString);
+        } catch (Exception $e){
+            echo "ERRO: data inválida <hr>";
+        }
+    }
+    if($mes == null){
+        setDate("$ano-01");
+        $dataAtual = new DateTime(date('Y-01-01'));
 
+        $diferenca = $data->diff($dataAtual);
+        echo("$diferenca->y anos");
+
+    }elseif($dia == null){
+        setDate("$ano-$mes");
+        $dataAtual = new DateTime(date('Y-m-1'));
+
+        $diferenca = $data->diff($dataAtual);
+        echo("$diferenca->y anos, $diferenca->m meses");
+    }else{
+        setDate("$ano-$mes-$dia");
+        $dataAtual = new DateTime(date('Y-m-d'));
+
+        $diferenca = $data->diff($dataAtual);
+        echo("$diferenca->y anos, $diferenca->m meses, $diferenca->m dias");
+    }
+
+
+/*
+    try {
+
+        $data = new DateTime("$ano-$mes-$dia");
+    } catch (Exception $e){
+        return "Data inválida.";
+    }*/
+
+    /*
     if($mes == null){
         $data = new DateTime("$ano-00-00");
         $dataAtual = new DateTime();
@@ -77,5 +118,6 @@ Route::get('/idade/{ano}/{mes?}/{dia?}', function ($ano, $mes=null, $dia=null) {
 
         return "idade = ". $diferenca->y . " anos, " . $diferenca->m . " meses" ;
     }
+    */
     
 });
